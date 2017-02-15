362bb3b625c0caab1bf474d059807d6299c5ba29
Jari Lehtinen <jari@feature.fi>
2017-02-15 09:36:16 +0200

    Very commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#fireant-uglify
+
+UglifyJS plugin for [Fireant](https://github.com/fireantjs/fireant)
---
+++ index.js
@@ -0,0 +1,38 @@
+var uglify = require('uglify-js');
+var chalk = require('chalk');
+var global = require('global');
+var fs = require('fs');
+
+var options = {
+    preserveComments: false,
+    compress: true,
+    mangle: true
+};
+
+function FireantUglify(files) {
+    if (global.options.uglify) {
+        options = global.options.uglify;
+    }
+
+    // Assign source
+    var source;
+
+    if (files) {
+        if (typeof files === 'string') {
+            source = fs.readFileSync(files);
+        } else {
+            for (var i = 0; i < files.length; i++) {
+                source = source + "\n" + fs.readFileSync(files[i]);
+            }
+        }
+    } else {
+        source = this.toString();
+    }
+
+    // Run UglifyJS
+    options.fromString = true;
+    return uglify.minify(source, options).code;
+}
+
+String.prototype.uglify = FireantUglify;
+module.exports = FireantUglify;
